f7d1f6c6263baf046e012460820ad63c583c31fc
amritagg <amritagg2137@gmail.com>
2021-01-02 18:37:39 +0530

    adding info file

--- app/src/main/java/com/example/mygallery/InfoActivity.java
+++ app/src/main/java/com/example/mygallery/InfoActivity.java
@@ -2,6 +2,7 @@ package com.example.mygallery;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
@@ -9,9 +10,15 @@ import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.widget.TextView;
+import org.jetbrains.annotations.NotNull;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class InfoActivity extends AppCompatActivity {
 
+    @SuppressLint("SetTextI18n")
     @RequiresApi(api = Build.VERSION_CODES.Q)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,24 +49,49 @@ public class InfoActivity extends AppCompatActivity {
             int heightColumn = cursor.getColumnIndex(MediaStore.Images.Media.HEIGHT);
             int widthColumn = cursor.getColumnIndex(MediaStore.Images.Media.WIDTH);
             int dataTakenColumn = cursor.getColumnIndex(MediaStore.Images.Media.DATE_TAKEN);
+            int pathColumn = cursor.getColumnIndex(MediaStore.Images.Media.RELATIVE_PATH);
 
             if (cursor.moveToFirst()) {
                 String name = cursor.getString(nameColumn);
                 int size = cursor.getInt(sizeColumn);
                 int height = cursor.getInt(heightColumn);
                 int width = cursor.getInt(widthColumn);
-                String dataTaken = cursor.getString(dataTakenColumn);
+                long dateTaken = cursor.getLong(dataTakenColumn);
+                String path_string = cursor.getString(pathColumn);
 
                 String dimension = height + "X" + width;
-                date_taken_view.setText(dataTaken);
+                date_taken_view.setText(timeConverter(dateTaken));
                 name_view.setText(name);
-                size_view.setText(size);
+                size_view.setText(sizeConverter(size));
                 dimension_view.setText(dimension);
-                path_view.setText((CharSequence)uri);
+                path_view.setText(path_string + name);
 
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
+    @NotNull
+    private String sizeConverter(int size){
+        float size_in_kb = (float) size/1024;
+        float size_in_mb = (float) size_in_kb/1024;
+
+        String kb_string = new DecimalFormat("##.##").format(size_in_kb);
+        String mb_string = new DecimalFormat("##.##").format(size_in_mb);
+        if( (int) size_in_kb/1024 == 0){
+            return kb_string + "KB";
+        }
+        return mb_string + "MB";
+    }
+
+    @NotNull
+    private String timeConverter(long time){
+        Date date = new Date(time);
+        @SuppressLint("SimpleDateFormat")
+        SimpleDateFormat formatted = new SimpleDateFormat("LLL dd, yyyy hh:mm:ss a");
+        formatted.setTimeZone(TimeZone.getTimeZone("GMT+5:30"));
+        return formatted.format(date);
+    }
+
 }
--- app/src/main/java/com/example/mygallery/ViewImageActivity.java
+++ app/src/main/java/com/example/mygallery/ViewImageActivity.java
@@ -1,14 +1,18 @@
 package com.example.mygallery;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
+
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
+
 import java.util.ArrayList;
 
 public class ViewImageActivity extends AppCompatActivity {
@@ -36,34 +40,49 @@ public class ViewImageActivity extends AppCompatActivity {
         viewPager.setCurrentItem(current);
     }
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        getMenuInflater().inflate(R.menu.bottom, menu);
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-//
-//        if(item.getItemId() == R.id.delete){
-//            deleteImage();
-//            return true;
-//        }else return super.onOptionsItemSelected(item);
-//    }
-//
-//    private void deleteImage() {
-//
-//        int currentID = viewPager.getCurrentItem();
-//        String url_string = image_uris.get(currentID);
-//        Uri uri = Uri.parse(url_string);
-//        int _id = getContentResolver().delete(
-//                uri,
-//                null,
-//                null
-//        );
-//        if(_id != 0){
-//            Toast.makeText(this, "Image deleted Successfully", Toast.LENGTH_SHORT).show();
-//        }
-//    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.bottom, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+         if(item.getItemId() == R.id.info){
+            int position = viewPager.getCurrentItem();
+            Intent intent = new Intent(this, InfoActivity.class);
+            intent.putExtra("Uri_String", image_uris.get(position));
+            startActivity(intent);
+            return true;
+         } else if(item.getItemId() == R.id.delete){
+             deleteImage();
+         }return super.onOptionsItemSelected(item);
+    }
+
+    private void deleteImage() {
+
+        int currentID = viewPager.getCurrentItem();
+        String url_string = image_uris.get(currentID);
+        final Uri uri = Uri.parse(url_string);
+
+        new AlertDialog.Builder(this)
+                .setTitle("Delete Image")
+                .setMessage("Do You really want to delete the image")
+                .setPositiveButton("YES!!", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getApplicationContext(), "The file " + uri + " is deleted", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNegativeButton("NO!", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getApplicationContext(), "The file " + uri + " will not be deleted", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .create()
+                .show();
+    }
 
 }
